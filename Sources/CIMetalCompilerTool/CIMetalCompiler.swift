400b3084abae79ba53b5b12fca92504991c8350f
dengweichao <dengweichao@hotmail.com>
2025-05-26 05:05:21 +0800

    Add more comments

--- Sources/CIMetalCompilerTool/CIMetalCompiler.swift
+++ Sources/CIMetalCompilerTool/CIMetalCompiler.swift
@@ -24,6 +24,9 @@ struct CIMetalCompilerTool: ParsableCommand {
         
         var airOutputs = [String]()
         
+        // First compile each input to .air files.
+        // Equivelent to this command line:
+        // xcrun metal -c -fcikernel MyKernel.metal -o MyKernel.air
         for input in inputs {
             let name = input.nameWithoutExtension
             
@@ -49,12 +52,15 @@ struct CIMetalCompilerTool: ParsableCommand {
         
         var metalLibs = [String]()
         
+        // Then, using metallib to link each .air file and output to a .metallib file.
+        // Equivelent to this command line:
+        // xcrun metallib --cikernel MyKernel.air -o MyKernel.metallib
         for airFile in airOutputs {
             let name = airFile.nameWithoutExtension
             
             print("linking \(airFile) to metallib")
             
-            let metalLibOutput = "\(cache)/\(name).cimetallib"
+            let metalLibOutput = "\(cache)/\(name).metallib"
             let p = Process()
             p.executableURL = xcRunURL
             p.arguments = [
@@ -73,6 +79,12 @@ struct CIMetalCompilerTool: ParsableCommand {
         
         print("merging \(metalLibs) to \(output)")
         
+        // Finally, merge all metallib files into one output file.
+        // Equivelent to this command line:
+        // xcrun metal -fcikernel -o MyKernel.metallib MyKernel1.metallib MyKernel2.metallib ...
+        // NOTE: This command is different from the one that was first introduced in WWDC20:
+        // https://developer.apple.com/videos/play/wwdc2020/10021
+        // The old command is obsolete and no longer works.
         let p = Process()
         p.executableURL = xcRunURL
         p.arguments = [
